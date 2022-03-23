70c9dec8a4818a808ab7ebce6274a91bd47c8689
FrozenKota <q38230kota@gmail.com>
2022-03-24 01:18:11 +0900

    動作OK:　GoogleMap ポリゴンオーバーレイ表示Example動作

--- App.tsx
+++ App.tsx
@@ -1,112 +1,150 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 
-import * as React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import MapView, {
+  Circle,
+  Polygon,
+  Polyline,
+  ProviderPropType,
+} from 'react-native-maps';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const { width, height } = Dimensions.get('window');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode:boolean = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+class Overlays extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      circle: {
+        center: {
+          latitude: LATITUDE + SPACE,
+          longitude: LONGITUDE + SPACE,
+        },
+        radius: 700,
+      },
+      polygon: [
+        {
+          latitude: LATITUDE + SPACE,
+          longitude: LONGITUDE + SPACE,
+        },
+        {
+          latitude: LATITUDE - SPACE,
+          longitude: LONGITUDE - SPACE,
+        },
+        {
+          latitude: LATITUDE - SPACE,
+          longitude: LONGITUDE + SPACE,
+        },
+      ],
+      polyline: [
+        {
+          latitude: LATITUDE + SPACE,
+          longitude: LONGITUDE - SPACE,
+        },
+        {
+          latitude: LATITUDE - 2 * SPACE,
+          longitude: LONGITUDE + 2 * SPACE,
+        },
+        {
+          latitude: LATITUDE - SPACE,
+          longitude: LONGITUDE - SPACE,
+        },
+        {
+          latitude: LATITUDE - 2 * SPACE,
+          longitude: LONGITUDE - SPACE,
+        },
+      ],
+    };
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  render() {
+    const { region, circle, polygon, polyline } = this.state;
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={this.props.provider}
+          style={styles.map}
+          initialRegion={region}
+        >
+          <Circle
+            center={circle.center}
+            radius={circle.radius}
+            fillColor="rgba(255, 255, 255, 1)"
+            strokeColor="rgba(0,0,0,0.5)"
+            zIndex={2}
+            strokeWidth={2}
+          />
+          <Polygon
+            coordinates={polygon}
+            fillColor="rgba(0, 200, 0, 0.5)"
+            strokeColor="rgba(0,0,0,0.5)"
+            strokeWidth={2}
+          />
+          <Polyline
+            coordinates={polyline}
+            strokeColor="rgba(0,0,200,0.5)"
+            strokeWidth={3}
+            lineDashPattern={[5, 2, 3, 2]}
+          />
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <View style={styles.bubble}>
+            <Text>Render circles, polygons, and polylines</Text>
+          </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </View>
+    );
+  }
+}
+
+Overlays.propTypes = {
+  provider: ProviderPropType,
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
   },
 });
 
-export default App;
+export default Overlays;
