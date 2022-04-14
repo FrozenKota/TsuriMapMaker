8345fa62b0c7ebd48f6a2824147da0997c666032
FrozenKota <q38230kota@gmail.com>
2022-04-14 19:12:21 +0900

    Assetコンポーネント　表示数のマジックナンバーを定数に置き換え

--- components/Asset.tsx
+++ components/Asset.tsx
@@ -7,37 +7,51 @@ const { width, height } = Dimensions.get('window');
 const AssetWindow = ({closeAssetHandler}: any) => {
     let data = [];
     const num = 123;
-    const IMAGE_SIZE = 4;
+    const ROW_IMAGE_NUM= 6;
 
-    for(let i = 0; i <= num; i+=4){
+    for(let i = 0; i <= num; i+=ROW_IMAGE_NUM){
         data.push(
             <View style={{flex:1, flexDirection: 'row'}}>
-                <TouchableOpacity style={{width: width/IMAGE_SIZE, height: width/IMAGE_SIZE}} onPress={closeAssetHandler}>
+                <TouchableOpacity style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}} onPress={closeAssetHandler}>
                     <Image
-                    style={{width: width/IMAGE_SIZE, height: width/IMAGE_SIZE}}
+                    style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}}
                     resizeMode='contain'
                     source={Images[i]}
                     />
                 </TouchableOpacity>
-                <TouchableOpacity style={{width: width/IMAGE_SIZE, height: width/IMAGE_SIZE}} onPress={closeAssetHandler}>
+                <TouchableOpacity style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}} onPress={closeAssetHandler}>
                     <Image
-                    style={{width: width/IMAGE_SIZE, height:width/IMAGE_SIZE}}
+                    style={{width: width/ROW_IMAGE_NUM, height:width/ROW_IMAGE_NUM}}
                     resizeMode='contain'
                     source={Images[i+1]}
                     />
                 </TouchableOpacity>
-                <TouchableOpacity style={{width: width/IMAGE_SIZE, height: width/IMAGE_SIZE}} onPress={closeAssetHandler}>
+                <TouchableOpacity style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}} onPress={closeAssetHandler}>
                     <Image
-                    style={{width: width/IMAGE_SIZE, height:width/IMAGE_SIZE}}
+                    style={{width: width/ROW_IMAGE_NUM, height:width/ROW_IMAGE_NUM}}
                     resizeMode='contain'
                     source={Images[i+3]}
                     />
                 </TouchableOpacity>
-                <TouchableOpacity style={{width: width/IMAGE_SIZE, height: width/IMAGE_SIZE}} onPress={closeAssetHandler}>
+                <TouchableOpacity style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}} onPress={closeAssetHandler}>
                     <Image
-                    style={{width: width/IMAGE_SIZE, height:width/IMAGE_SIZE}}
+                    style={{width: width/ROW_IMAGE_NUM, height:width/ROW_IMAGE_NUM}}
                     resizeMode='contain'
                     source={Images[i+4]}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}} onPress={closeAssetHandler}>
+                    <Image
+                    style={{width: width/ROW_IMAGE_NUM, height:width/ROW_IMAGE_NUM}}
+                    resizeMode='contain'
+                    source={Images[i+5]}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={{width: width/ROW_IMAGE_NUM, height: width/ROW_IMAGE_NUM}} onPress={closeAssetHandler}>
+                    <Image
+                    style={{width: width/ROW_IMAGE_NUM, height:width/ROW_IMAGE_NUM}}
+                    resizeMode='contain'
+                    source={Images[i+6]}
                     />
                 </TouchableOpacity>
             </View>
@@ -46,7 +60,7 @@ const AssetWindow = ({closeAssetHandler}: any) => {
 
     return(
         <View style={styles.assetView}>
-            <ScrollView style={{}}>
+            <ScrollView>
                 {data}
             </ScrollView>
         </View>
